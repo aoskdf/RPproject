42dd19bfb67a90401b73173cf0f67651ec02f463
aoskdf <siy3352@nate.com>
2021-06-18 15:49:59 +0100

    2LED handle

---
+++ MultiLED.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+LED1 = 12
+LED2 = 10
+
+def main():
+    GPIO.setup(LED1, GPIO.OUT, initial=GPIO.LOW)
+    
+    GPIO.setup(LED2, GPIO.OUT, initial=GPIO.LOW)
+    while 1:
+        GPIO.output(LED1, GPIO.HIGH)
+        GPIO.output(LED2, GPIO.LOW)
+        time.sleep(1)
+        
+        GPIO.output(LED1, GPIO.LOW)
+        GPIO.output(LED2, GPIO.HIGH)
+        time.sleep(1)
+
+if __name__=='__main__':
+    main()
